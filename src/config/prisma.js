71f0f95ba492870c0fbbb2215016773edc002a71
Filipe Carlitos Alfredo <alfredofilipecarlitos@gmail.com>
2026-04-27 12:37:10 -0700

    Update prisma.js

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,7 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient({https://github.com/Fica-eng/sige-mec-backend/edit/main/src/prisma.js
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
