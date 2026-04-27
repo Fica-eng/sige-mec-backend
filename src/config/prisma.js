0827b590ea9f5331e9e22218fea947ee0d00c9dd
Filipe Carlitos Alfredo <alfredofilipecarlitos@gmail.com>
2026-04-27 12:18:17 -0700

    Update and rename src/prisma.js to src/config/prisma.js

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient({
+const prisma = new PrismaClient({https://github.com/Fica-eng/sige-mec-backend/edit/main/src/prisma.js
   log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
 });
 
